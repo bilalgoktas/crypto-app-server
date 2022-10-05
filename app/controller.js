0e28ef462334cbda6e6a30297b73c63323896347
bilalgoktas <bgoktas97@gmail.com>
2022-10-05 16:18:44 +0200

    commented out fetching, added instead fake data

--- app/controller.js
+++ app/controller.js
@@ -1,37 +1,43 @@
 import fetch from "node-fetch";
+import allPrices from "../fake-data/allPrices.json" assert { type: "json" };
+import oneMetaData from "../fake-data/oneMetaData.json" assert { type: "json" };
+import onePrice from "../fake-data/onePrice.json" assert { type: "json" };
 
 export const readAllPrices = async (req, res) => {
-  const response = await fetch(
-    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-    { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
-  );
-  const data = await response.json();
+  //   const response = await fetch(
+  //     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+  //     { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
+  //   );
+  //   const data = await response.json();
 
-  res.status(200).json(data).end();
+  //   res.status(200).json(data).end();
+  res.status(200).send(allPrices);
 };
 
 export const readOneMetaData = async (req, res) => {
-  const response = await fetch(
-    "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?" +
-      new URLSearchParams({
-        id: req.params.id,
-      }),
-    { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
-  );
-  const data = await response.json();
+  //   const response = await fetch(
+  //     "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?" +
+  //       new URLSearchParams({
+  //         id: req.params.id,
+  //       }),
+  //     { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
+  //   );
+  //   const data = await response.json();
 
-  res.status(200).json(data).end();
+  //   res.status(200).json(data).end();
+  res.status(200).send(oneMetaData);
 };
 
 export const readOnePrice = async (req, res) => {
-  const response = await fetch(
-    "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?" +
-      new URLSearchParams({
-        id: req.params.id,
-      }),
-    { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
-  );
-  const data = await response.json();
+  //   const response = await fetch(
+  //     "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?" +
+  //       new URLSearchParams({
+  //         id: req.params.id,
+  //       }),
+  //     { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
+  //   );
+  //   const data = await response.json();
 
-  res.status(200).json(data).end();
+  //   res.status(200).json(data).end();
+  res.status(200).send(onePrice);
 };
