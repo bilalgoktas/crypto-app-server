b006d98d58322d914629e8d0a3cf8ee2323f8b80
bilalgoktas <bgoktas97@gmail.com>
2022-10-05 15:15:41 +0200

    created server, routes and controller

---
+++ app/routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import * as controller from "./controller.js";
+
+const router = express.Router();
+
+router.get("/all", controller.readAll);
+router.get("/trendings", controller.readTrendings);
+router.get("/info/:id", controller.readOneInfo);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,5 @@
+import app from "./app/app.js";
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server listetning at port ${port}`));
