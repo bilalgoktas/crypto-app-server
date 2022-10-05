7e071cebccfce4aacc8036f59fbf6d1c12830b40
bilalgoktas <bgoktas97@gmail.com>
2022-10-05 16:07:47 +0200

    added read one price funciton, changed function names

--- app/controller.js
+++ app/controller.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-export const readAll = async (req, res) => {
+export const readAllPrices = async (req, res) => {
   const response = await fetch(
     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
     { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
@@ -10,9 +10,22 @@ export const readAll = async (req, res) => {
   res.status(200).json(data).end();
 };
 
-export const readOneInfo = async (req, res) => {
+export const readOneMetaData = async (req, res) => {
   const response = await fetch(
-    `https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?` +
+    "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?" +
+      new URLSearchParams({
+        id: req.params.id,
+      }),
+    { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
+  );
+  const data = await response.json();
+
+  res.status(200).json(data).end();
+};
+
+export const readOnePrice = async (req, res) => {
+  const response = await fetch(
+    "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?" +
       new URLSearchParams({
         id: req.params.id,
       }),
--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,8 @@ import * as controller from "./controller.js";
 
 const router = express.Router();
 
-router.get("/all", controller.readAll);
-router.get("/info/:id", controller.readOneInfo);
+router.get("/all", controller.readAllPrices);
+router.get("/metadata/:id", controller.readOneMetaData);
+router.get("/price/:id", controller.readOnePrice);
 
 export default router;
