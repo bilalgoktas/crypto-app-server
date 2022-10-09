cf10545f2ecd3918ccadb833b8fee3ed604b788d
Bilal Goktas <bgoktas97@gmail.com>
2022-10-09 16:17:55 +0200

    updated routes and controller for category filter

--- app/controller.js
+++ app/controller.js
@@ -4,42 +4,43 @@ import oneMetaData from "../fake-data/oneMetaData.json";
 import onePrice from "../fake-data/onePrice.json";
 import topTwenty from "../fake-data/topTwenty.json";
 
-export const readAllPrices = async (req, res) => {
-  //   const response = await fetch(
-  //     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-  //     { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
-  //   );
-  //   const data = await response.json();
+export const readPricesFromCategory = async (req, res) => {
+  // const response = await fetch(
+  //   "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?cryptocurrency_type=" +
+  //     new URLSearchParams({ category: req.params.category }),
+  //   { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
+  // );
+  // const data = await response.json();
 
-  //   res.status(200).json(data).end();
+  // res.status(200).json(data).end();
   res.status(200).send(allPrices);
 };
 
 export const readOneMetaData = async (req, res) => {
-  //   const response = await fetch(
-  //     "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?" +
-  //       new URLSearchParams({
-  //         id: req.params.id,
-  //       }),
-  //     { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
-  //   );
-  //   const data = await response.json();
+  // const response = await fetch(
+  //   "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?" +
+  //     new URLSearchParams({
+  //       id: req.params.id,
+  //     }),
+  //   { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
+  // );
+  // const data = await response.json();
 
-  //   res.status(200).json(data).end();
+  // res.status(200).json(data).end();
   res.status(200).send(oneMetaData);
 };
 
 export const readOnePrice = async (req, res) => {
-  //   const response = await fetch(
-  //     "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?" +
-  //       new URLSearchParams({
-  //         id: req.params.id,
-  //       }),
-  //     { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
-  //   );
-  //   const data = await response.json();
+  // const response = await fetch(
+  //   "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?" +
+  //     new URLSearchParams({
+  //       id: req.params.id,
+  //     }),
+  //   { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
+  // );
+  // const data = await response.json();
 
-  //   res.status(200).json(data).end();
+  // res.status(200).json(data).end();
   res.status(200).send(onePrice);
 };
 
--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ import * as controller from "./controller.js";
 
 const router = express.Router();
 
-router.get("/all", controller.readAllPrices);
+router.get("/cryptos/:category", controller.readAllPrices);
 router.get("/metadata/:id", controller.readOneMetaData);
 router.get("/price/:id", controller.readOnePrice);
 router.get("/top20", controller.readTopTwenty);
