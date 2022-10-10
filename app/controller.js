9a67e06a7b46ea091350f4bbf91fcf88d9f2932c
Bilal Goktas <bgoktas97@gmail.com>
2022-10-10 23:52:32 +0200

    added fake datas for specific use cases, updated routes and controllers

--- app/controller.js
+++ app/controller.js
@@ -1,49 +1,54 @@
 import fetch from "node-fetch";
 import allPrices from "../fake-data/allPrices.json";
+import metaDatas from "../fake-data/metaDatas.json";
+import popPrices from "../fake-data/popPrices.json";
+import pricesToFilter from "../fake-data/pricesToFilter.json";
 import oneMetaData from "../fake-data/oneMetaData.json";
 import onePrice from "../fake-data/onePrice.json";
 import topTwenty from "../fake-data/topTwenty.json";
 
-export const readPricesFromCategory = async (req, res) => {
+export const readAllPrices = async (req, res) => {
   // const response = await fetch(
   //   "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?" +
-  //     new URLSearchParams({ cryptocurrency_type: req.params.category }),
-  //   { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
-  // );
-  // const data = await response.json();
-  // res.status(200).json(data).end();
-  // res.status(200).send(allPrices);
-};
-
-export const readOneMetaData = async (req, res) => {
-  const response = await fetch(
-    "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?" +
-      new URLSearchParams({
-        id: req.params.id,
-      }),
-    { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
-  );
-  const data = await response.json();
-
-  // res.status(200).json(data).end();
-  res.status(200).send(oneMetaData);
-};
-
-export const readOnePrice = async (req, res) => {
-  // const response = await fetch(
-  //   "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?" +
   //     new URLSearchParams({
-  //       id: req.params.id,
+  //       cryptocurrency_type: req.params.category,
+  //       limit: 10,
+  //       start: (req.params.currentPage - 1) * 10 + 1,
+  //       convert: req.query.convert,
   //     }),
   //   { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
   // );
   // const data = await response.json();
-
   // res.status(200).json(data).end();
-  res.status(200).send(onePrice);
+
+  res.status(200).send(allPrices);
 };
 
-export const readTopTwenty = async (req, res) => {
+export const readMetaData = async (req, res) => {
+  // const response = await fetch(
+  //   `https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${req.query.id}`,
+  //   { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
+  // );
+  // const data = await response.json();
+  // res.status(200).json(data).end();
+  const data = metaDatas[req.params.id];
+
+  res.status(200).send(data);
+};
+
+export const readPrice = (req, res) => {
+  const data = pricesToFilter[req.query.id];
+
+  res.status(200).send(data);
+};
+
+export const readPopPrice = (req, res) => {
+  const data = popPrices[req.query.id];
+
+  res.status(200).send(data);
+};
+
+export const readTopTen = async (req, res) => {
   // const response = await fetch(
   //   `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=20&convert=${req.query.convert}`,
   //   { headers: { "X-CMC_PRO_API_KEY": "21c7dee4-29ae-4443-a620-d0bfbfcfc8ea" } }
@@ -51,5 +56,6 @@ export const readTopTwenty = async (req, res) => {
   // const data = await response.json();
 
   // res.status(200).json(data).end();
-  res.status(200).send(topTwenty);
+  const data = allPrices.slice(0, 10);
+  res.status(200).send(data);
 };
--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,10 @@ import * as controller from "./controller.js";
 
 const router = express.Router();
 
-router.get("/cryptos/:category", controller.readPricesFromCategory);
-router.get("/metadata/:id", controller.readOneMetaData);
-router.get("/price/:id", controller.readOnePrice);
-router.get("/top20", controller.readTopTwenty);
+router.get("/cryptos", controller.readAllPrices);
+router.get("/metadata/:id", controller.readMetaData);
+router.get("/popprice", controller.readPopPrice);
+router.get("/price", controller.readPrice);
+router.get("/top10", controller.readTopTen);
 
 export default router;
